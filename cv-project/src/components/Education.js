64302835a598a6b1515fbabf6db6193b2b7ba746
Tyler Schnabel <tylerwschnabel@gmail.com>
2022-10-01 09:19:41 -0700

    Added edit inputs

--- cv-project/src/components/Education.js
+++ cv-project/src/components/Education.js
@@ -11,17 +11,22 @@ const Education = (props) => {
                 return <div className="degree" id={degree.id} key={degree.id}>
                             <div className="school">
                                 <h3>{degree.school}</h3>
+                                <input type='text' className='editInputs' id={"schoolInupt-"+degree.id} defaultValue={degree.school}></input>
                             </div>
                             <div className="major">
                                 <p>Major :</p>
                                 <p>{degree.major}</p>
+                                <input type='text' className='editInputs' id={"majorInupt-"+degree.id} defaultValue={degree.major}></input>
                             </div>
                             <div className="minor">
                                 <p>Minor :</p>
                                 <p>{degree.minor}</p>
+                                <input type='text' className='editInputs' id={"minorInupt-"+degree.id} defaultValue={degree.minor}></input>
                             </div>
                             <div className="degreeStartDate">
                                 <p>{degree.startDate} to {degree.endDate}</p>
+                                <input type='date' className='editInputs' id={"startDateInupt-"+degree.id} defaultValue={degree.startDate}></input>
+                                <input type='date' className='editInputs' id={"endDateInupt-"+degree.id} defaultValue={degree.endDate}></input>
                             </div>
                             <div className="resumeBtns">
                                 <button className="editEducation" >Edit Education</button>
